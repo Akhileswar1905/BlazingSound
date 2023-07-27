de477492463b91cde54d7bd95b31f76231aa3524
Akhileswar <akhileswarsathivada@gmail.com>
2023-07-27 17:58:36 +0530

    27thjuly

--- songs.session.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO songs (name, img, link)
-VALUES (
-        'After Hours',
-        'https://upload.wikimedia.org/wikipedia/en/3/39/The_Weeknd_-_Starboy.png',
-        'https://firebasestorage.googleapis.com/v0/b/blazingsound-32357.appspot.com/o/%5BiSongs.info%5D%2002%20-%20Inthandham.mp3?alt=media&token=501a162a-5ee8-411e-b078-d1ea91143bf2'
-    );
